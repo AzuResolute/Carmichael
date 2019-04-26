c803f895b07833964c4f09405f00da6018c65cb9
Jan Czarina Javier <janczarinajavier@Jans-MacBook-Pro.local>
2019-04-26 18:10:43 -0400

    Completed Report Components

--- utilities/index.js
+++ utilities/index.js
@@ -1,3 +1,13 @@
-export const DateExcel2JavaScript = function (date) {
-  return new Date((date - (25567 + 1)) * 86400 * 1000);
+const DateExcel2JavaScript = function (date) {
+  let day = new Date((date - (25567)) * 86400 * 1000);
+  if(Object.prototype.toString.call(day) === "[object Date]") {
+    if (isNaN(day.getTime())) {  // d.valueOf() could also work
+      // date is not valid
+    } else {
+      return day
+      // date is valid
+    }
+  }
 }
+
+module.exports = {DateExcel2JavaScript}

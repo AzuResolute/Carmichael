f5627e1c72f723703f31e283661b3dd3fe598530
AzuResolute <roger.palabasan@baruchmail.cuny.edu>
2019-08-30 13:15:41 -0400

    Adding Online Excel Dependency

--- utilities/Links.js
+++ utilities/Links.js
@@ -1,5 +1,6 @@
 const NorthwindPath =
-  'C:/Users/roger/Documents/Projects/Insight/utilities/Northwind.xls'
+  // 'C:/Users/roger/Documents/Projects/Insight/utilities/Northwind.xls'
+  'https://github.com/AzuResolute/Carmichael/raw/master/utilities/Northwind.xls'
 const OrderRequestPath =
   'C:/Users/roger/Documents/Projects/Insight/utilities/OrderRequest.xlsx'
 const UpdateInventoryPath =
